60efd1c8f37b153b9e906700924d57b776556484
coranos <coranos@users.noreply.github.com>
2022-09-07 17:58:55 -0500

    debugging dist over time

--- index.js
+++ index.js
@@ -8,68 +8,124 @@
 // variables
 
 // functions
-const getDate = (ts) => {
-  // console.log('getDate', ts);
-  return new Date(ts)
-      .toISOString()
-      .replace('T', ' ')
-      .replace('.000Z', '');
-};
-const getDistributionOverTime = async (httpsRateLimit, historyChunkSize, timeChunkSizeMs, account) => {
-  let previous;
+
+const getDistributionOverTime = async (httpsRateLimit, historyChunkSize, timeChunkFn, knownAccountTypeMap, sourceAccount) => {
+  let next;
   let stop = false;
-  const rawByTimeChunk = new Map();
+
+
+  // console.log('knownAccountTypeMap', knownAccountTypeMap);
+
+  const amountByTimeChunkAndTypeMap = new Map();
+  const processedBlockHashSet = new Set();
   while (!stop) {
     // by default stop.
     stop = true;
-    const req = {
-      action: 'account_history',
-      account: account,
-      count: historyChunkSize,
-    };
-    if (previous) {
-      req.previous = previous;
-    }
-    const resp = await httpsRateLimit.sendRequest(req);
-    if (resp.history) {
-      if (resp.history.length > 0) {
-        previous = undefined;
-        resp.history.forEach((historyElt) => {
-          // console.log('historyElt', historyElt);
-          const amount = BigInt(historyElt.amount);
-          const timeMs = historyElt.local_timestamp * 1000;
-          // console.log('local_timestamp', getDate(timeMs));
-          const localTimeChunk = Math.floor(timeMs / timeChunkSizeMs);
-          // console.log('localTimeChunk', localTimeChunk);
-          if (rawByTimeChunk.has(localTimeChunk)) {
-            const newAmount = amount + rawByTimeChunk.get(localTimeChunk);
-            rawByTimeChunk.set(localTimeChunk, newAmount);
-          } else {
-            rawByTimeChunk.set(localTimeChunk, amount);
-          }
+    const addHistoryFn = async () => {
+      const accountHistoryReq = {
+        action: 'account_history',
+        account: sourceAccount,
+        count: historyChunkSize,
+        reverse: 'true',
+        raw: 'true',
+      };
+      if (next) {
+        req.head = next;
+      }
 
-          previous = historyElt.previous;
-        });
-        // if there's more history, get it.
-        if (previous !== undefined) {
-          stop = false;
+      // do not reuse next hash
+      next = undefined;
+
+      const accountHistoryResp = await httpsRateLimit.sendRequest(accountHistoryReq);
+      // console.log('accountHistoryResp', accountHistoryResp);
+      if (accountHistoryResp.history) {
+        if (accountHistoryResp.history.length > 0) {
+          for (const historyElt of accountHistoryResp.history) {
+          // for (let historyIx = 0; historyIx < accountHistoryResp.history.length; historyIx++) {
+            // const historyElt = accountHistoryResp.history[historyIx];
+            // console.log('historyElt', historyElt);
+            if (!processedBlockHashSet.has(historyElt.hash)) {
+              processedBlockHashSet.add(historyElt.hash);
+              if (historyElt.type == 'send') {
+                const destAccount = historyElt.account;
+                const amount = parseFloat(historyElt.amount_decimal);
+                const timeMs = historyElt.local_timestamp * 1000;
+                // console.log('local_timestamp', getDate(timeMs));
+                const localTimeChunk = timeChunkFn(timeMs);
+                // console.log('historyElt.hash', historyElt.hash, localTimeChunk);
+                // console.log('localTimeChunk', localTimeChunk);
+                let amountByTypeMap;
+                if (amountByTimeChunkAndTypeMap.has(localTimeChunk)) {
+                  amountByTypeMap = amountByTimeChunkAndTypeMap.get(localTimeChunk);
+                } else {
+                  amountByTypeMap = new Map();
+                  amountByTimeChunkAndTypeMap.set(localTimeChunk, amountByTypeMap);
+                }
+
+                let type = 'unknown';
+                if (knownAccountTypeMap.has(destAccount)) {
+                  type = knownAccountTypeMap.get(destAccount);
+                }
+                if (type == 'unknown') {
+                  const accountInfoReq = {
+                    action: 'account_info',
+                    account: destAccount,
+                    representative: true,
+                    weight: true,
+                    pending: true,
+                  };
+                  const accountInfoResp = await httpsRateLimit.sendRequest(accountInfoReq);
+                  // console.log('accountInfoResp', accountInfoResp);
+                  if (accountInfoResp.representative == 'ban_1tipbotgges3ss8pso6xf76gsyqnb69uwcxcyhouym67z7ofefy1jz7kepoy') {
+                    type = 'tipbot';
+                    // save type so we dont ahve to redo API call.
+                    knownAccountTypeMap.set(destAccount, type);
+                  }
+                }
+
+                if (type == 'unknown') {
+                  // const senderType = knownAccountTypeMap.get(sourceAccount);
+                  // console.log('unknown account', senderType, '=>', destAccount, amount, localTimeChunk, historyElt);
+                  // throw Error('unknown account');
+                  knownAccountTypeMap.set(destAccount, 'gray');
+                }
+
+                let oldAmount = 0.0;
+                if (amountByTypeMap.has(type)) {
+                  oldAmount = amountByTypeMap.get(type);
+                }
+                const newAmount = oldAmount + amount;
+                amountByTypeMap.set(type, newAmount);
+              }
+            }
+
+            next = historyElt.next;
+          };
+
+          // if there's more history, get it.
+          if (next !== undefined) {
+            stop = false;
+          }
         }
       }
-    }
+    };
+    await addHistoryFn();
   }
   const histogram = [];
-  for (const [chunk, count] of rawByTimeChunk) {
-    const key = getDate(chunk*timeChunkSizeMs);
-    histogram.push({
-      bucket: key,
-      count: count.toString(),
-    });
+  for (const [timeChunk, amountByTypeMap] of amountByTimeChunkAndTypeMap) {
+    for (const [accountType, amount] of amountByTypeMap) {
+      histogram.push({
+        timeChunk: timeChunk,
+        accountType: accountType,
+        amount: amount.toFixed(2),
+      });
+    }
   }
 
   // loggingUtil.log(dateUtil.getDate(), 'histogramMap', histogramMap);
   // loggingUtil.log(dateUtil.getDate(), 'histogram', JSON.stringify(histogram));
 
-  return {    account: account,histogram:histogram};
+  return {account: sourceAccount, histogram: histogram};
 };
 
 exports.getDistributionOverTime = getDistributionOverTime;
--- main.js
+++ main.js
@@ -9,17 +9,71 @@ const run = async () => {
     console.log('#usage:');
     console.log('npm start <url>');
   } else {
-    const genesis = 'ban_1bananobh5rat99qfgt1ptpieie5swmoth87thi74qgbfrij7dcgjiij94xr';
     const historyChunkSize = 1000;
     // chunk into days
-    const timeChunkSizeMs = 1000 * 60 * 60 * 24 * 365;
+    const timeChunkFn = (ts) => {
+      const isoStr = new Date(ts)
+          .toISOString();
+      const yearMonth = isoStr.substring(0, 7);
+      return yearMonth;
+    };
+    const knownAccountTypeMap = new Map();
+
+    const knownAccountsUrl = 'https://api.creeper.banano.cc/banano/v1/known/accounts';
+    // console.log('knownAccountsUrl', knownAccountsUrl);
+    httpsRateLimit.setUrl(knownAccountsUrl);
+    const knownAccountsResponse = await httpsRateLimit.sendRequest({includeType: true});
+    // console.log('knownAccountsResponse', knownAccountsResponse);
+    knownAccountsResponse.forEach((knownAccountElt) => {
+      const account = knownAccountElt.address;
+      const type = knownAccountElt.type;
+      switch (type) {
+        case 'distribution':
+        case 'faucet':
+        case 'event':
+          knownAccountTypeMap.set(account, 'source');
+          break;
+        case 'exchange':
+        case 'burn':
+        case 'donation':
+        case 'gambling':
+        case 'service':
+          knownAccountTypeMap.set(account, 'sink');
+          break;
+        case 'representative':
+        case 'team-member':
+          knownAccountTypeMap.set(account, 'gray');
+          break;
+        default:
+          console.log('unknown account type', type, knownAccountElt);
+      }
+    });
+    // for (const [account, type] of knownAccountTypeMap) {
+    //   console.log('known account type', account, type);
+    // }
 
     const url = process.argv[2];
-    console.log('url', url);
+    // console.log('url', url);
     httpsRateLimit.setUrl(url);
 
-    const distributionOverTime = await index.getDistributionOverTime(httpsRateLimit, historyChunkSize, timeChunkSizeMs, genesis);
-    console.log('distributionOverTime', distributionOverTime);
+    const distribution = [];
+
+    const knownAccountTypeList = [];
+    for (const [account, type] of knownAccountTypeMap) {
+      knownAccountTypeList.push({account: account, type: type});
+    }
+
+    for (const knownAccountType of knownAccountTypeList) {
+      const account = knownAccountType.account;
+      const type = knownAccountType.type;
+      console.log('distribution calculation STARTING', account, distribution.length, 'of', knownAccountTypeList.length);
+      if (type != 'gray') {
+        const distributionOverTime = await index.getDistributionOverTime(httpsRateLimit, historyChunkSize, timeChunkFn, knownAccountTypeMap, account);
+        distribution.push(distributionOverTime);
+      }
+      console.log('distribution calculation FINISHED', account, distribution.length, 'of', knownAccountTypeList.length);
+    }
+    console.log('distribution', distribution);
   }
 };
 
