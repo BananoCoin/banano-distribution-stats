d568d8d7e2bf5e0648afd021fa8fc9703067651a
coranos <coranos@users.noreply.github.com>
2022-10-17 20:06:48 -0500

    adding a post-exchange tier

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ const getDistributionOverTime = async (httpsRateLimit, historyChunkSize,
 
             const destAccount = historyElt.account;
             let destType = 'distributed-to-' + tier;
+            if(srcType == 'exchange') {
+              destType = 'exchanged-to-' + tier;
+            }
             if (knownAccountTypeMap.has(destAccount)) {
               destType = knownAccountTypeMap.get(destAccount);
             } else {
